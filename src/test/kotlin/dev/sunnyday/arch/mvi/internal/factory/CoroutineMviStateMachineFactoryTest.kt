4ed4dd7bb360f3d7a5fc924798dc741087b50daa
Alexandr Tcikin <av@sunnyday.dev>
2023-03-18 16:39:05 +0300

    arch-mvi-14 StateMachineFactory tests improved

--- src/test/kotlin/dev/sunnyday/arch/mvi/internal/factory/CoroutineMviStateMachineFactoryTest.kt
+++ src/test/kotlin/dev/sunnyday/arch/mvi/internal/factory/CoroutineMviStateMachineFactoryTest.kt
@@ -5,12 +5,17 @@ import dev.sunnyday.arch.mvi.StateTransition
 import dev.sunnyday.arch.mvi.StateTransitionListener
 import dev.sunnyday.arch.mvi.Update
 import dev.sunnyday.arch.mvi.internal.StateMachineImpl
+import dev.sunnyday.arch.mvi.internal.coroutine.MviCoroutineScope
+import dev.sunnyday.arch.mvi.internal.coroutine.isMviCoroutineScope
+import dev.sunnyday.arch.mvi.test.ConstructorRule
+import dev.sunnyday.arch.mvi.test.ConstructorRule.Companion.Matcher
 import dev.sunnyday.arch.mvi.test.Event
 import dev.sunnyday.arch.mvi.test.SideEffect
 import dev.sunnyday.arch.mvi.test.State
 import io.mockk.*
 import kotlinx.coroutines.CoroutineScope
 import org.junit.jupiter.api.Test
+import kotlin.coroutines.EmptyCoroutineContext
 
 class CoroutineMviStateMachineFactoryTest {
 
@@ -19,25 +24,62 @@ class CoroutineMviStateMachineFactoryTest {
         val state = State()
         val reducer = Reducer<State, Event, Update<State, SideEffect>> { _, _ -> Update.nothing() }
 
-        every { constructed(state, reducer).onEvent(any()) } returns Unit
-
-        CoroutineMviStateMachineFactory.createStateMachine(
-            initialState = state,
-            reducer = reducer,
-        ).onEvent(Event())
-
-        verify { constructed(state, reducer).onEvent(any()) }
-    }
-
-    private fun MockKMatcherScope.constructed(
-        state: State,
-        reducer: Reducer<State, Event, Update<State, SideEffect>>,
-    ): StateMachineImpl<State, Event, SideEffect> {
-        return constructedWith(
+        val rule = ConstructorRule.create<StateMachineImpl<State, Event, SideEffect>>(
             EqMatcher(state, true),
-            ConstantMatcher<CoroutineScope>(true),
+            Matcher<CoroutineScope> { it.isMviCoroutineScope },
             EqMatcher(reducer, true),
             ConstantMatcher<StateTransitionListener<StateTransition<State, Event, SideEffect>>>(true)
         )
+
+        val obj = CoroutineMviStateMachineFactory.createStateMachine(
+            initialState = state,
+            reducer = reducer,
+        )
+
+        rule.verifyConstructorCalled(obj)
+    }
+
+    @Test
+    fun `construct state machine with mvi coroutine scope`() = mockkConstructor(StateMachineImpl::class) {
+        val state = State()
+        val reducer = Reducer<State, Event, Update<State, SideEffect>> { _, _ -> Update.nothing() }
+        val coroutineScope = MviCoroutineScope()
+
+        val rule = ConstructorRule.create<StateMachineImpl<State, Event, SideEffect>>(
+            EqMatcher(state, true),
+            EqMatcher(coroutineScope, true),
+            EqMatcher(reducer, true),
+            ConstantMatcher<StateTransitionListener<StateTransition<State, Event, SideEffect>>>(true)
+        )
+
+        val obj = CoroutineMviStateMachineFactory.createStateMachine(
+            initialState = state,
+            reducer = reducer,
+            coroutineScope = coroutineScope,
+        )
+
+        rule.verifyConstructorCalled(obj)
+    }
+
+    @Test
+    fun `construct state machine with any coroutine scope`() = mockkConstructor(StateMachineImpl::class) {
+        val state = State()
+        val reducer = Reducer<State, Event, Update<State, SideEffect>> { _, _ -> Update.nothing() }
+        val coroutineScope = CoroutineScope(EmptyCoroutineContext)
+
+        val rule = ConstructorRule.create<StateMachineImpl<State, Event, SideEffect>>(
+            EqMatcher(state, true),
+            Matcher<CoroutineScope> { it !== coroutineScope && it.isMviCoroutineScope },
+            EqMatcher(reducer, true),
+            ConstantMatcher<StateTransitionListener<StateTransition<State, Event, SideEffect>>>(true)
+        )
+
+        val obj = CoroutineMviStateMachineFactory.createStateMachine(
+            initialState = state,
+            reducer = reducer,
+            coroutineScope = coroutineScope,
+        )
+
+        rule.verifyConstructorCalled(obj)
     }
 }
